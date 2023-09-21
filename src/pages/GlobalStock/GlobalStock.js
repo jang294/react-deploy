f09027f0ad2632d50483505144ae0ee3460e3485
minjeong <cookie00421@gmail.com>
2023-09-21 15:49:25 +0900

    리액트 라우터 추가, 해외 주식 페이지 추가

--- src/pages/GlobalStock/GlobalStock.js
+++ src/pages/GlobalStock/GlobalStock.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import styles from "./Home.module.css";
+import styles from "./GlobalStock.module.css";
 import HeaderCard from "../../assets/images/header_1.svg";
-import ArrowIcon from "../../assets/images/ic_arrow.svg";
 import LinkListItem from "../../components/LinkListItem/LinkListItem";
 import EventLogo1 from "../../assets/images/event_logo_1.png";
 import EventInfo from "../../components/EventInfo/EventInfo";
 import AccordianListItem from "../../components/AccordianListItem/AccordianListItem";
+import EventCard from "../../components/EventCard/EventCard";
 import TopNav from "../../components/TopNav/TopNav";
 import { useNavigate } from "react-router-dom";
 
@@ -13,8 +13,29 @@ const events = [
   { num: 1, title: "기간", text: "2023.07.01(토) ~ 2023.12.31(일)" },
   { num: 3, title: "대상", text: "신한투자증권 생애 첫 계좌 개설 신규 고객" },
 ];
+const eventCards = [
+  {
+    cardImage: HeaderCard,
+    title: (
+      <>
+        온라인 국내주식 수수료
+        <br />
+        <span className={styles.primaryColor}>평생 혜택 </span>제공
+      </>
+    ),
+    list: [
+      "* 유관기관 제비용만 고객 부담(07.01 기준)",
+      "- 코스피, 코스닥, 코넥스 : 0.00363960%",
+      "- K-OTC : 0.09091870%",
+      "- ETF, ETN, ELW : 0.00420870%",
 
-const Home = () => {
+      "* 온라인 채널 거래에 한함(증권플러스 제외)",
+    ],
+  },
+  { num: 3, title: "대상", text: "신한투자증권 생애 첫 계좌 개설 신규 고객" },
+];
+
+const GlobalStock = () => {
   const navigate = useNavigate();
   const [openAccordion1, setOpenAccordion1] = useState(false);
   const [openAccordion2, setOpenAccordion2] = useState(false);
@@ -29,39 +50,20 @@ const Home = () => {
   return (
     <>
       <TopNav />
+      {/*상단 탭 */}
       <div className={styles.tabMenuContainer}>
         <button onClick={() => navigate("/")}>국내 주식</button>
         <button onClick={() => navigate("/global")}>해외 주식</button>
       </div>
+
       <div className={styles.container}>
         <section className={styles.header}>
-          <div className={styles.headerSubTitle}>지금 계좌 개설하면</div>
+          <div className={styles.headerSubTitle}>지금이 일본 투자할 타이밍</div>
           <div className={styles.headerTitle}>
-            <span className={styles.primaryColor}>수수료 평생혜택</span> 제공
+            <span className={styles.primaryColor}>수수료 + 환전우대 혜택!</span>
           </div>
           {/* 카드 섹션 */}
-          <div className={styles.cardContainer}>
-            <div className={styles.cardImageContainer}>
-              <img
-                className={styles.cardImage}
-                src={HeaderCard}
-                alt="온라인 수수료 평생 무료 혜택"
-              />
-            </div>
-            <div className={styles.cardTitle}>
-              온라인 국내주식 수수료
-              <br />
-              <span className={styles.primaryColor}>평생 혜택 </span>제공
-            </div>
-            {/* 설명 */}
-            <ul className={styles.cardDesc}>
-              <li>* 유관기관 제비용만 고객 부담(07.01 기준)</li>
-              <li>- 코스피, 코스닥, 코넥스 : 0.00363960%</li>
-              <li>- K-OTC : 0.09091870%</li>
-              <li>- ETF, ETN, ELW : 0.00420870%</li>
-              <li>* 온라인 채널 거래에 한함(증권플러스 제외)</li>
-            </ul>
-          </div>
+          <EventCard event={eventCards[0]} />
 
           {/* 기간/대상 */}
           <div className={styles.infoContainer}>
@@ -134,4 +136,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default GlobalStock;
